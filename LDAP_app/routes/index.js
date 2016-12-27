5f169ef7bdd37f01ee253023e4ef9caa9b5ef0f3
epi <vinoep@gmail.com>
2016-12-26 19:46:43 -0800

    May be it was git error, was missing function keywords and had messed up.Fixed index.js

--- LDAP_app/routes/index.js
+++ LDAP_app/routes/index.js
@@ -6,32 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'LDAP APP' });
 });
 
-router.get('/about',(req,res)=>{
+router.get('/about',function(req,res){
 	res.render('about',{title:'About'});
-})
+});
 
-router.get('/contact',(req,res)=>{
+router.get('/contact',function(req,res){
 	res.render('contact',{title:'Contact Us'});
-})
-router.get('/adminUser',(req,res)=>{
+});
+router.get('/adminUser',function(req,res){
 console.log(req.body);
 res.render('adminUser',{title:'Admin Home Page'});
 });
 
-router.get('/regularUser',(req,res)=>{
+router.get('/regularUser',function(req,res){
 	res.render('regularUser',{title: 'User Home Page'})
-})
+});
 
-router.get('/newUserData',(req,res)=>{
+router.get('/newUserData',function(req,res){
 res.render('newUserData',{title: 'Create New User'
 });
 console.log(req.body);
-})
+});
 
-router.get('/createUser',(req,res)=>{
+router.get('/createUser',function(req,res){
 	res.render('createUser',{title: 'Successfully created'});
 	console.log(req.body);
-})
+});
 
 
 module.exports = router;
